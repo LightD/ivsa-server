0b508e06b75f4da7487584dc552b48266497c5bb
Light Dream <nourforgive@gmail.com>
2017-01-26 00:41:04 +0800

    update vapor package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "ivsa",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 2),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
         .Package(url: "https://github.com/vapor/mongo-provider.git", majorVersion: 1, minor: 1)
         
     ],
